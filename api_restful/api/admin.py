75c712f5d576888e9b463a4adabc16cc5406146a
thehanatos <admin4523@gmail.com>
2025-04-11 09:22:46 +0200

    add into admin the models

--- api_restful/api/admin.py
+++ api_restful/api/admin.py
@@ -1,9 +1,23 @@
 from django.contrib import admin
 
-from .models import CustomUser
+from .models import CustomUser, Project, Issue, Comment
 
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
     list_display = ['id', 'username', 'email', 'age', 'can_be_contacted', 'can_data_be_shared']
     search_fields = ['username', 'email']
     list_filter = ['can_be_contacted', 'can_data_be_shared']
+    
+
+
+@admin.register(Project)
+class ProjetAdmin(admin.ModelAdmin):
+    list_display = ['title']
+
+@admin.register(Issue)
+class IssueAdmin(admin.ModelAdmin):
+    list_display = ['title']
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['description']
