5cf07c48ecb1561c3d20dac6239a5934bcd9a485
thehanatos <admin4523@gmail.com>
2025-04-11 16:26:22 +0200

    hash password

--- api_restful/api/serializers.py
+++ api_restful/api/serializers.py
@@ -8,6 +8,17 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {
             'password': {'write_only': True}, 
         }
+    def create(self, validated_data):
+        return CustomUser.objects.create_user(**validated_data)
+    
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        if password:
+            instance.set_password(password)
+        instance.save()
+        return instance
 
 class ContributorSerializer(serializers.ModelSerializer):
     class Meta:
